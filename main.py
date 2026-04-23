1b964573eb3caa86eea7f63eddf1d07adfc90268
sudo-del-System32 <franciscodanielbertinidealmeida@yahoo.com.br>
2026-04-23 09:50:54 -0300

    refact: for better redability

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
-import pygame
-from re import match
-
-PATTERN = r"^[0-9]+\.?[0-9]*$"
-
-massa: float = 5
-velocidade: float = 4
-campoMagnetico: float = 0.5
-carga: float = 12.52131
-
+from src.entradas.startMenu import start
 
 def main():
-    while(1):
-        x = input()
-        if match(PATTERN, x):
-            x = float(x)
-            print(x)
-        else:
-            print("ERRO PRECISA SER UM NUMERO REAL!")
-
+    start()
 
 if __name__ == "__main__":
     main()
