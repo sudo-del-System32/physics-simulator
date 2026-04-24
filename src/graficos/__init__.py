cf3f6e0e1c2a22c42dbd694503e6ab83317472e1
Cadu <cadumalmeida13@gmail.com>
2026-04-24 13:18:19 -0300

    cores escolhidados e dados mocaos

--- src/graficos/__init__.py
+++ src/graficos/__init__.py
@@ -1,19 +1,23 @@
 import matplotlib.pyplot as plt
 
-# graficos iniciais dos resultaods da equação com os input
-valores = [13, 43, 23.4, 12]
-variaveis = ['massa', 'velocidade', 'campo magnetico', 'carga']
-bars = plt.bar(variaveis, valores, color=["#9a840a", '#054f99', '#0d4502', '#790111'])
-plt.bar_label(bars, padding=3)
-plt.ylim(0, max(valores) + 5) 
+# campos e valores
+categorias = ['massa', 'velocidade', 'campo magnetico', 'carga', 'força magnetica', 'raio', 'AC']
+valores = [13, 43, 23.4, 12, 54, 34, 18]
+cores = ['#f99201', "#b80538", "#6905a3", "#0CC1C4", "#ce09b1", "#aeff17bf", '#ff0022']
+
+# customização
+fig, ax = plt.subplots()
+fig.patch.set_facecolor("#12263a")
+ax.set_facecolor('#12263a')
+ax.tick_params(colors='white')
+ax.spines['bottom'].set_color('white')
+ax.spines['left'].set_color('white')
+ax.spines['top'].set_color('#1e1e1e')   
+ax.spines['right'].set_color('#1e1e1e') 
+bars = ax.bar(categorias, valores, color=cores)  
+ax.bar_label(bars, padding=3, color='white')  
+ax.set_ylim(0, max(valores) + 10)            
+
+# apresentação
 plt.show()
 
-
-
-# graficos posteriores de força
-valores = [ 54, 34, 18]
-variaveis = ['força magnetica', 'raio', 'AC']
-bars = plt.bar(variaveis, valores, color=[ '#054f99', "#188802", '#F91900'])
-plt.bar_label(bars, padding=3)
-plt.ylim(0, max(valores) + 5) 
-plt.show()
